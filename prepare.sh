a7bcc01d5606162f239079bf103ac2556f53a994
Jarred Masterson <jarred.masterson@gmail.com>
2021-09-05 19:19:30 -0600

    Typeo fix

--- prepare.sh
+++ prepare.sh
@@ -4,4 +4,4 @@ mkdir /etc/local/runonce.d
 mkdir /etc/local/runonce.d/ran
 mv /prov/northrack/runonce.sh /usr/local/bin/runonce.sh
 echo "@reboot root /usr/local/bin/runonce.sh"
-mv /prov/northrack/northrack-anisble.sh /etc/local/runonce.d/northrack-ansible.sh
+mv /prov/northrack/northrack-ansible.sh /etc/local/runonce.d/northrack-ansible.sh
