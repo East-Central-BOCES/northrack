76b8c9d55b2cf6c63e5713d0a050c012c0fdbbb5
Jarred Masterson <jarred.masterson@gmail.com>
2021-09-05 21:46:36 -0600

    echo to crontab file

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,5 @@ mkdir /etc/local
 mkdir /etc/local/runonce.d
 mkdir /etc/local/runonce.d/ran
 mv /prov/northrack/runonce.sh /usr/local/bin/runonce.sh
-echo "@reboot root /usr/local/bin/runonce.sh"
+echo "@reboot root /usr/local/bin/runonce.sh" >> /etc/crontab
 mv /prov/northrack/northrack-ansible.sh /etc/local/runonce.d/northrack-ansible.sh
