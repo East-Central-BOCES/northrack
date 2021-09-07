3ba0f96441b6d1491f9508a203398082cbdbd451
jarredm <jarredm@ecboces.org>
2021-09-07 10:17:42 -0600

    make shell scripts executable after move

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,7 @@ mkdir /etc/local
 mkdir /etc/local/runonce.d
 mkdir /etc/local/runonce.d/ran
 mv /prov/northrack/runonce.sh /usr/local/bin/runonce.sh
+chmod +x /usr/local/bin/runonce.sh
 echo "@reboot root /usr/local/bin/runonce.sh" >> /etc/crontab
 mv /prov/northrack/northrack-ansible.sh /etc/local/runonce.d/northrack-ansible.sh
+chmod +x /etc/local/runonce.d/northrack-ansible.sh
