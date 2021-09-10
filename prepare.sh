5f8fe340604ef097ecfb882180e02ede5521c498
jarredm <jarredm@ecboces.org>
2021-09-10 17:12:33 -0600

    Disabling runonce scripts again

--- prepare.sh
+++ prepare.sh
@@ -5,5 +5,5 @@ mkdir /etc/local/runonce.d/ran
 mv /prov/northrack/runonce.sh /usr/local/bin/runonce.sh
 chmod +x /usr/local/bin/runonce.sh
 echo "@reboot root /usr/local/bin/runonce.sh" >> /etc/crontab
-mv /prov/northrack/pausenreboot.sh /etc/local/runonce.d/pausenreboot.sh
-chmod +x /etc/local/runonce.d/pausenreboot.sh
+# mv /prov/northrack/pausenreboot.sh /etc/local/runonce.d/pausenreboot.sh
+# chmod +x /etc/local/runonce.d/pausenreboot.sh
