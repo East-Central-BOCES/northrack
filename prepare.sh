0c912126fffede23fcbb4f127219a840bb143f7f
jarredm <jarredm@ecboces.org>
2021-09-10 14:09:32 -0600

    Put the user back in crontab job. I must be tired today

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,6 @@ mkdir /etc/local/runonce.d
 mkdir /etc/local/runonce.d/ran
 mv /prov/northrack/runonce.sh /usr/local/bin/runonce.sh
 chmod +x /usr/local/bin/runonce.sh
-echo "@reboot /usr/local/bin/runonce.sh" >> /etc/crontab
+echo "@reboot root /usr/local/bin/runonce.sh" >> /etc/crontab
 mv /prov/northrack/pausenreboot.sh /etc/local/runonce.d/pausenreboot.sh
 chmod +x /etc/local/runonce.d/pausenreboot.sh
