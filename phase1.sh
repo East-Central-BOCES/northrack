55562f6d562718183072617fd4fc9085e39d52a4
Jarred Masterson <jarred.masterson@gmail.com>
2021-10-10 11:04:49 -0600

    Install gluster via shell

--- phase1.sh
+++ phase1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 pause 30
 ansible-playbook /prov/northrack/phase1.yml > /etc/local/runonce.d/ran/phase1.log 2>&1
+dnf -y install glusterfs-server >> /etc/local/ranonce.d/ran/phase1.log 2>&1
 #mv /prov/northrack/northrack-ansible.sh /etc/local/runonce.d/northrack-ansible.sh
 #chmod +x /etc/local/runonce.d/northrack-ansible.sh
 shutdown -r now
