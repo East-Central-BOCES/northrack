e96c0c83a052c89853274268c53b6bf863fb34d3
Jarred Masterson <jarred.masterson@gmail.com>
2021-10-10 12:49:42 -0600

    Enable northrack-ansible.sh

--- phase1.sh
+++ phase1.sh
@@ -2,6 +2,6 @@
 sleep 30
 ansible-playbook /prov/northrack/phase1.yml > /etc/local/runonce.d/ran/phase1.log 2>&1
 dnf -y install glusterfs-server >> /etc/local/runonce.d/ran/phase1.log 2>&1
-#mv /prov/northrack/northrack-ansible.sh /etc/local/runonce.d/northrack-ansible.sh
-#chmod +x /etc/local/runonce.d/northrack-ansible.sh
+mv /prov/northrack/northrack-ansible.sh /etc/local/runonce.d/northrack-ansible.sh
+chmod +x /etc/local/runonce.d/northrack-ansible.sh
 shutdown -r now
