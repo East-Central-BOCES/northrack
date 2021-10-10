c3d1f7fb96062e647093ec9a313905c61be0e969
Jarred Masterson <jarred.masterson@gmail.com>
2021-10-10 16:16:20 -0600

    Use shell instead of ansible for package installs

--- northrack-ansible.sh
+++ northrack-ansible.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 sleep 30
-dnf -y install https://resources.ovirt.org/pub/yum-repo/ovirt-release44.rpm
-ansible-playbook /prov/northrack/northrack.yml > /etc/local/runonce.d/ran/northrack.log 2>&1
+{
+  dnf -y install https://resources.ovirt.org/pub/yum-repo/ovirt-release44.rpm
+  dnf -y install sanlock
+  dnf -y install ovirt-hosted-engine-setup
+  dnf -y install cockpit-ovirt-dashboard
+  dnf -y install vdsm-gluster
+  dnf -y install ovirt-host
+  dnf -y install gluster-ansible-roles
+  ansible-playbook /prov/northrack/northrack.yml
+} >> /etc/local/runonce.d/ran/northrack.log 2>&1
 shutdown -r now
