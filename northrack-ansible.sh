060943974206469ec1c8ee9fc40a66c96b1d5435
Jarred Masterson <jarred.masterson@gmail.com>
2021-10-10 13:45:55 -0600

    Add ovirt repo

--- northrack-ansible.sh
+++ northrack-ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sleep 30
+dnf -y install https://resources.ovirt.org/pub/yum-repo/ovirt-release44.rpm
 ansible-playbook /prov/northrack/northrack.yml > /etc/local/runonce.d/ran/northrack.log 2>&1
 shutdown -r now
