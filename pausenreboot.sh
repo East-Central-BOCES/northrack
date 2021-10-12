e4dde8504912ee76c59fbaf877d58777fecc9fac
Jarred Masterson <jarred.masterson@gmail.com>
2021-10-12 01:34:19 -0600

    Reduce initial sleep

--- pausenreboot.sh
+++ pausenreboot.sh
@@ -3,6 +3,6 @@ logger -n 10.10.10.10 "Launching pausenreboot.sh"
 mv /prov/northrack/phase1.sh /etc/local/runonce.d/phase1.sh
 chmod +x /etc/local/runonce.d/phase1.sh
 logger -n 10.10.10.10 "Moved phase1.sh Pausing..."
-sleep 180
+sleep 120
 logger -n 10.10.10.10 "Rebooting from pausenreboot.sh"
 shutdown -r now
